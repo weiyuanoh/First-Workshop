bce053c36b07f9cf9796aa6c29e01230529a92d0
weiyuanoh <weiyuannnn@gmail.com>
2022-09-08 09:48:10 +0800

    First Commit

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import numpy as np
 import pandas as pd
 
 
-
-df= pd.read_csv("BTC-USD.csv")
+# visualising the first data set (you have to put the same the data file in to the same file as your python project)
+df = pd.read_csv('BTC-USD.csv')
 print(df.head())
+
+
+df.plot ( x = "Date", y = "Close", kind = "line", figsize =(15,8), title = "BTC Price", xlabel = "Date", ylabel = "Price")
+
+plt.show()
 
 
