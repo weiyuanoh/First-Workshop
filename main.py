f583acf9e300c7681eec0009d5b5a4ecc16f0cf0
weiyuanoh <weiyuannnn@gmail.com>
2022-09-21 15:17:16 +0800

    First Commit

--- main.py
+++ main.py
@@ -4,14 +4,38 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-
 # visualising the first data set (you have to put the same the data file in to the same file as your python project)
-df = pd.read_csv('BTC-USD.csv')
-print(df.head())
+df = pd.read_csv('BTC-USD1.csv')  # is to init excel into dataframe represented by df
+
+# plotting time series data
+# df.plot(x="Date", y="Close", kind="line", figsize=(15, 8), title="BTC Price", xlabel="Date", ylabel="Price")
+# plt.show()
 
 
-df.plot ( x = "Date", y = "Close", kind = "line", figsize =(15,8), title = "BTC Price", xlabel = "Date", ylabel = "Price")
+# SMA 20
+df['SMA_20'] = df['Close'].rolling(window=20, min_periods=1).mean()
+# SMA 50
+df['SMA_50'] = df['Close'].rolling(window=50, min_periods=1).mean()
+# creating list to close
+close = df['Close'].values.tolist()
+print(close)
+print(sum(close))
 
-plt.show()
 
+# calculating variance
+def variance(close):
+    n = len(close)
+    mean = sum(close) / n
+    variance = 0
+    for close in close:
+        dev = close - mean
+        variance += (dev ** 2)/n
 
+    return variance
+varclose = variance(close)
+print(varclose)
+
+# plot SMA
+# df.plot(x="Date", y=["Close", "SMA_20", "SMA_50"], kind="line", figsize=(15, 8), title="BTC Price", xlabel="Date",
+#        ylabel="Price")
+# plt.show()
